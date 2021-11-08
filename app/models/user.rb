c7df17e5f67f7e8ac2cca0caff78a07d2d620e28
Pwako11 <akolo@hotmail.com>
2021-11-08 16:10:38 -0500

    added model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
-    
+
+    has_many :dishes, dependent: :destroy
+
+    validates_uniqueness_of :email, :username
+    validates_presence_of :name, :username, :email, :password_digest
 end
