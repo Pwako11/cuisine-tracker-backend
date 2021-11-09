bebcd4207bbb8d10d857117218566361496fcd3a
Pwako11 <akolo@hotmail.com>
2021-11-08 20:57:08 -0500

    updated associations to include many regions  through dishes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :dishes, dependent: :destroy
+    has_many :regions, through: :dishes
 
     validates_uniqueness_of :email, :username
     validates_presence_of :name, :username, :email, :password_digest
